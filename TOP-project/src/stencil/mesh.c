05d70f3b72f344be3861db6a50e20d60ed9949bf
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-02 15:54:10 +0200

    mesh adapted to cache blocking in solve_jacobi

--- TOP-project/src/stencil/mesh.c
+++ TOP-project/src/stencil/mesh.c
@@ -3,10 +3,15 @@
 
 #include <assert.h>
 #include <stdlib.h>
-#include <omp.h>
+
+#define BLOCK_SIZE_I 32   // Adapt L1
+#define BLOCK_SIZE_J 256  // Adapt L2
+#define BLOCK_SIZE_K 4    // Adapt L3
 
 mesh_t mesh_new(usz dim_x, usz dim_y, usz dim_z, mesh_kind_t kind) {
     usz const ghost_size = 2 * STENCIL_ORDER;
+
+    // Calculate the total number of cells
     usz total_cells = (dim_x + ghost_size) * (dim_y + ghost_size) * (dim_z + ghost_size);
 
     // Allocate memory for all cells at once
@@ -24,22 +29,43 @@ mesh_t mesh_new(usz dim_x, usz dim_y, usz dim_z, mesh_kind_t kind) {
         return (mesh_t){0};
     }
 
-    // Setup pointers
-    for (usz i = 0; i < dim_x + ghost_size; ++i) {
-        cells[i] = malloc((dim_y + ghost_size) * sizeof(cell_t*));
-        if (NULL == cells[i]) {
-            // Cleanup previously allocated memory
-            for (usz k = 0; k < i; k++) {
-                free(cells[k]);
-            }
-            free(cells);
-            free(all_cells);
-            error("failed to allocate second-level pointers for mesh");
-            return (mesh_t){0};
-        }
+    // Setup pointers in a blocked manner
+    for (usz i = 0; i < dim_x + ghost_size; i += BLOCK_SIZE_I) {
+        for (usz j = 0; j < dim_y + ghost_size; j += BLOCK_SIZE_J) {
+            for (usz k = 0; k < dim_z + ghost_size; k += BLOCK_SIZE_K) {
+                // Allocate memory for the block of pointers
+                cell_t** block_ptr = malloc(BLOCK_SIZE_I * sizeof(cell_t*));
+                if (NULL == block_ptr) {
+                    // Cleanup previously allocated memory
+                    for (usz bi = 0; bi < i; bi += BLOCK_SIZE_I) {
+                        for (usz bj = 0; bj < j; bj += BLOCK_SIZE_J) {
+                            free(cells[bi][bj]);
+                        }
+                    }
+                    for (usz bi = 0; bi < i; bi += BLOCK_SIZE_I) {
+                        free(cells[bi]);
+                    }
+                    free(cells);
+                    free(all_cells);
+                    error("failed to allocate memory for block pointers");
+                    return (mesh_t){0};
+                }
 
-        for (usz j = 0; j < dim_y + ghost_size; ++j) {
-            cells[i][j] = &all_cells[(i * (dim_y + ghost_size) + j) * (dim_z + ghost_size)];
+                // Setup pointers within the block
+                for (usz bi = i; bi < i + BLOCK_SIZE_I && bi < dim_x + ghost_size; ++bi) {
+                    for (usz bj = j; bj < j + BLOCK_SIZE_J && bj < dim_y + ghost_size; ++bj) {
+                        // Calculate the index within the linear array
+                        usz linear_idx = ((bi * (dim_y + ghost_size)) + bj) * (dim_z + ghost_size) + k;
+
+                        // Set the pointer to the corresponding cell in the linear array
+                        block_ptr[bi - i] = &all_cells[linear_idx];
+                    }
+                }
+
+                // Assign the block pointer to the appropriate position in the cells array
+                cells[i / BLOCK_SIZE_I][(j / BLOCK_SIZE_J) * ((dim_x + 2 * STENCIL_ORDER) / BLOCK_SIZE_I) + k / BLOCK_SIZE_K] = block_ptr;
+
+            }
         }
     }
 
@@ -54,9 +80,12 @@ mesh_t mesh_new(usz dim_x, usz dim_y, usz dim_z, mesh_kind_t kind) {
 
 void mesh_drop(mesh_t* self) {
     if (NULL != self->cells) {
-        free(self->cells[0][0]); // Free all cells at once
-        for (usz i = 0; i < self->dim_x; ++i) {
-            free(self->cells[i]);
+        // Free memory for all cells at once
+        free(self->cells[0][0]);
+        for (usz i = 0; i < (self->dim_x + 2 * STENCIL_ORDER) / BLOCK_SIZE_I; ++i) {
+            for (usz j = 0; j < (self->dim_y + 2 * STENCIL_ORDER) / BLOCK_SIZE_J; ++j) {
+                free(self->cells[i][j]);
+            }
         }
         free(self->cells);
     }
