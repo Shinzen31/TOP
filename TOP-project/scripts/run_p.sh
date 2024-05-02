1eae592fee03a44b585232401d9730d610a50f84
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-02 22:20:04 +0200

    update scripts

--- TOP-project/scripts/run_p.sh
+++ TOP-project/scripts/run_p.sh
@@ -16,9 +16,7 @@ perf report >> ../speed_up/perf_500.txt
 perf record ./my_executable ../config_1000.txt ../results/res_1000.txt
 perf report >> ../speed_up/perf_1000.txt
 
-python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100.txt >> ../speed_up/perf_100.txt
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500.txt >> ../speed_up/perf_500.txt
-python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000.txt >> ../speed_up/perf_1000.txt
+
 
 cd ../scripts
 ./clean.sh
