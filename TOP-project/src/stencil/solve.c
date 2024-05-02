7d0f88210702cf50086183873f18ce4e15ae61ad
Shinzen <83638956+Shinzen31@users.noreply.github.com>
2024-05-02 18:27:09 +0200

    Update solve.c
    ajout cache blocking et  OpenMP MPI

--- TOP-project/src/stencil/solve.c
+++ TOP-project/src/stencil/solve.c
@@ -2,10 +2,11 @@
 
 #include <assert.h>
 #include <math.h>
+#include <omp.h> // Inclusion de la bibliothèque OpenMP
 
-#define BLOCK_SIZE_I 32   //adapt L1
-#define BLOCK_SIZE_J 256  //adapt L2
-#define BLOCK_SIZE_K 4    //adapt L3
+#define BLOCK_SIZE_I 32   // Taille de bloc pour s'adapter à la mémoire cache L1
+#define BLOCK_SIZE_J 256  // Taille de bloc pour s'adapter à la mémoire cache L2
+#define BLOCK_SIZE_K 4    // Taille de bloc pour s'adapter à la mémoire cache L3
 
 void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     assert(A->dim_x == B->dim_x && B->dim_x == C->dim_x);
@@ -17,10 +18,11 @@ void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     usz const dim_z = A->dim_z;
     usz i, j, k, o, bi, bj, bk;
 
+    #pragma omp parallel for private(i, j, k, bi, bj, bk, o) // Activation de la parallélisation
     for (k = STENCIL_ORDER; k < dim_z - STENCIL_ORDER; k += BLOCK_SIZE_K) {
         for (j = STENCIL_ORDER; j < dim_y - STENCIL_ORDER; j += BLOCK_SIZE_J) {
             for (i = STENCIL_ORDER; i < dim_x - STENCIL_ORDER; i += BLOCK_SIZE_I) {
-                // loop unrolling and fission
+                // Déroulement de boucle et division
                 for (bk = k; bk < k + BLOCK_SIZE_K && bk < dim_z - STENCIL_ORDER; ++bk) {
                     for (bj = j; bj < j + BLOCK_SIZE_J && bj < dim_y - STENCIL_ORDER; ++bj) {
                         for (bi = i; bi < i + BLOCK_SIZE_I && bi < dim_x - STENCIL_ORDER; ++bi) {
@@ -43,4 +45,3 @@ void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
 
     mesh_copy_core(A, C);
 }
-
