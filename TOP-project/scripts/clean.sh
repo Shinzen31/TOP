217839da112838182bb091a7907f691371d33e2f
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-03 06:09:27 +0200

    correct init, improve scripts

--- TOP-project/scripts/clean.sh
+++ TOP-project/scripts/clean.sh
@@ -3,4 +3,5 @@
 rm -f ../src/cmake_install.cmake ../src/CMakeCache.txt ../src/Makefile ../src/my_executable
 find ../src -maxdepth 1 -type f -name '*.dylib' -delete
 find ../src -maxdepth 1 -type f -name '*.so' -delete
+find ../src -maxdepth 1 -type f -name 'perf.da*' -delete
 rm -rf ../src/CMakeFiles
--- TOP-project/src/stencil/init.c
+++ TOP-project/src/stencil/init.c
@@ -1,54 +1,45 @@
 #include "stencil/init.h"
+
 #include "stencil/comm_handler.h"
 #include "stencil/mesh.h"
 
 #include <assert.h>
 #include <math.h>
-#include <omp.h>
 
-// Cache blocking sizes
-#define BLOCK_SIZE_I 32
-#define BLOCK_SIZE_J 256
-#define BLOCK_SIZE_K 4
+#include <omp.h>
 
 static f64 compute_core_pressure(usz i, usz j, usz k) {
     return sin((f64)k * cos((f64)i + 0.311) * cos((f64)j + 0.817) + 0.613);
 }
 
-// Optimized setup_mesh_cell_values with cache blocking
 static void setup_mesh_cell_values(mesh_t* mesh, comm_handler_t const* comm_handler) {
     #pragma omp parallel for collapse(3)
-    for (usz k = 0; k < mesh->dim_z; k += BLOCK_SIZE_K) {
-        for (usz j = 0; j < mesh->dim_y; j += BLOCK_SIZE_J) {
-            for (usz i = 0; i < mesh->dim_x; i += BLOCK_SIZE_I) {
-                for (usz bk = k; bk < k + BLOCK_SIZE_K && bk < mesh->dim_z; ++bk) {
-                    for (usz bj = j; bj < j + BLOCK_SIZE_J && bj < mesh->dim_y; ++bj) {
-                        for (usz bi = i; bi < i + BLOCK_SIZE_I && bi < mesh->dim_x; ++bi) {
-                            switch (mesh->kind) {
-                                case MESH_KIND_CONSTANT:
-                                    mesh->cells[bi][bj][bk].value = compute_core_pressure(
-                                        comm_handler->coord_x + bi,
-                                        comm_handler->coord_y + bj,
-                                        comm_handler->coord_z + bk
-                                    );
-                                    break;
-                                case MESH_KIND_INPUT:
-                                    if ((bi >= STENCIL_ORDER && (bi < mesh->dim_x - STENCIL_ORDER)) &&
-                                        (bj >= STENCIL_ORDER && (bj < mesh->dim_y - STENCIL_ORDER)) &&
-                                        (bk >= STENCIL_ORDER && (bk < mesh->dim_z - STENCIL_ORDER))) {
-                                        mesh->cells[bi][bj][bk].value = 1.0;
-                                    } else {
-                                        mesh->cells[bi][bj][bk].value = 0.0;
-                                    }
-                                    break;
-                                case MESH_KIND_OUTPUT:
-                                    mesh->cells[bi][bj][bk].value = 0.0;
-                                    break;
-                                default:
-                                    __builtin_unreachable();
-                            }
+    for (usz i = 0; i < mesh->dim_x; ++i) {
+        for (usz j = 0; j < mesh->dim_y; ++j) {
+            for (usz k = 0; k < mesh->dim_z; ++k) {
+                switch (mesh->kind) {
+                    case MESH_KIND_CONSTANT:
+                        mesh->cells[i][j][k].value = compute_core_pressure(
+                            comm_handler->coord_x + i,
+                            comm_handler->coord_y + j,
+                            comm_handler->coord_z + k
+                        );
+                        break;
+                    case MESH_KIND_INPUT:
+                        if ((i >= STENCIL_ORDER && (i < mesh->dim_x - STENCIL_ORDER)) &&
+                            (j >= STENCIL_ORDER && (j < mesh->dim_y - STENCIL_ORDER)) &&
+                            (k >= STENCIL_ORDER && (k < mesh->dim_z - STENCIL_ORDER)))
+                        {
+                            mesh->cells[i][j][k].value = 1.0;
+                        } else {
+                            mesh->cells[i][j][k].value = 0.0;
                         }
-                    }
+                        break;
+                    case MESH_KIND_OUTPUT:
+                        mesh->cells[i][j][k].value = 0.0;
+                        break;
+                    default:
+                        __builtin_unreachable();
                 }
             }
         }
@@ -80,38 +71,11 @@ void init_meshes(mesh_t* A, mesh_t* B, mesh_t* C, comm_handler_t const* comm_han
         C->dim_z == comm_handler->loc_dim_z + STENCIL_ORDER * 2
     );
 
-    // Setup mesh cell values and kinds for each mesh
-    #pragma omp parallel for collapse(3)
-    for (usz i = 0; i < A->dim_x; ++i) {
-        for (usz j = 0; j < A->dim_y; ++j) {
-            for (usz k = 0; k < A->dim_z; ++k) {
-                // Compute cell value based on mesh kind
-                switch (A->kind) {
-                    case MESH_KIND_CONSTANT:
-                        A->cells[i][j][k].value = compute_core_pressure(
-                            comm_handler->coord_x + i - STENCIL_ORDER,
-                            comm_handler->coord_y + j - STENCIL_ORDER,
-                            comm_handler->coord_z + k - STENCIL_ORDER
-                        );
-                        break;
-                    case MESH_KIND_INPUT:
-                        A->cells[i][j][k].value = (i >= STENCIL_ORDER && i < A->dim_x - STENCIL_ORDER &&
-                                                   j >= STENCIL_ORDER && j < A->dim_y - STENCIL_ORDER &&
-                                                   k >= STENCIL_ORDER && k < A->dim_z - STENCIL_ORDER) ? 1.0 : 0.0;
-                        break;
-                    case MESH_KIND_OUTPUT:
-                        A->cells[i][j][k].value = 0.0;
-                        break;
-                    default:
-                        assert(0 && "Invalid mesh kind");
-                }
-                // Set cell kind based on position in mesh
-                A->cells[i][j][k].kind = mesh_set_cell_kind(A, i, j, k);
-            }
-        }
-    }
+    setup_mesh_cell_kinds(A);
+    setup_mesh_cell_kinds(B);
+    setup_mesh_cell_kinds(C);
 
-    // Copy initialized mesh A to meshes B and C
-    mesh_copy_core(B, A);
-    mesh_copy_core(C, A);
+    setup_mesh_cell_values(A, comm_handler);
+    setup_mesh_cell_values(B, comm_handler);
+    setup_mesh_cell_values(C, comm_handler);
 }
