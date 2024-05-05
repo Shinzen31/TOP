72f35b220ad0d220fff2c1470b0eb572fd50e02a
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-05 16:44:59 +0200

    Ofast

--- TOP-project/scripts/run_p.sh
+++ TOP-project/scripts/run_p.sh
@@ -7,15 +7,15 @@ cd ../src
 cmake .
 make
 
-i=1
-perf record ./my_executable ../config.txt ../results/res_100_$i.txt
-perf report > ../speed_up/perf_100_$i.txt
+i=24
+perf record ./my_executable ../config.txt ../results/res_100_Ofast_$i.txt
+perf report > ../speed_up/perf_100_Ofast_$i.txt
 
-perf record ./my_executable ../config_500.txt ../results/res_500_$i.txt
-perf report > ../speed_up/perf_500_$i.txt
+perf record ./my_executable ../config_500.txt ../results/res_500_Ofast_$i.txt
+perf report > ../speed_up/perf_500_Ofast_$i.txt
 
-perf record ./my_executable ../config_1000.txt ../results/res_1000_$i.txt
-perf report > ../speed_up/perf_1000_$i.txt
+perf record ./my_executable ../config_1000.txt ../results/res_1000_Ofast_$i.txt
+perf report > ../speed_up/perf_1000__Ofast_$i.txt
 
 cd ../scripts
 ./clean.sh
