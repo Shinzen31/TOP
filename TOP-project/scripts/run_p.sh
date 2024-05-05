99fc153dd35f1f6285632fd4a7e65901eddd0a34
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-05 17:22:47 +0200

    speed up -Ofast

--- TOP-project/scripts/run_p.sh
+++ TOP-project/scripts/run_p.sh
@@ -15,7 +15,7 @@ perf record ./my_executable ../config_500.txt ../results/res_500_Ofast_$i.txt
 perf report > ../speed_up/perf_500_Ofast_$i.txt
 
 perf record ./my_executable ../config_1000.txt ../results/res_1000_Ofast_$i.txt
-perf report > ../speed_up/perf_1000__Ofast_$i.txt
+perf report > ../speed_up/perf_1000_Ofast_$i.txt
 
 cd ../scripts
 ./clean.sh
