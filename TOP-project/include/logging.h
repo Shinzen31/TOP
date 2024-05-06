783619b27c6cc02c3aed2467b08e339d17564397
yizhi0129 <bailiyunzhong@gmail.com>
2024-05-07 00:06:24 +0200

    result threags 48

--- TOP-project/include/logging.h
+++ TOP-project/include/logging.h
@@ -5,16 +5,16 @@
 
 #define info(fmt, ...)                                                                             \
     do {                                                                                           \
-        fprintf(stderr, "\x1b[1;36m[INFO]:\x1b[0m " fmt "\n", ##__VA_ARGS__);                      \
+        fprintf(stderr, "\x1b[1;36m[INFO]:\x1b[0m " fmt "\n", __VA_ARGS__);                      \
     } while (0)
 
 #define warn(fmt, ...)                                                                             \
     do {                                                                                           \
-        fprintf(stderr, "\x1b[1;33m[WARNING]:\x1b[0m " fmt "\n", ##__VA_ARGS__);                   \
+        fprintf(stderr, "\x1b[1;33m[WARNING]:\x1b[0m " fmt "\n", __VA_ARGS__);                   \
     } while (0)
 
 #define error(fmt, ...)                                                                            \
     do {                                                                                           \
-        fprintf(stderr, "\x1b[1;31m[ERROR]:\x1b[0m " fmt "\n", ##__VA_ARGS__);                     \
+        fprintf(stderr, "\x1b[1;31m[ERROR]:\x1b[0m " fmt "\n", __VA_ARGS__);                     \
         exit(-1);                                                                                  \
     } while (0)
