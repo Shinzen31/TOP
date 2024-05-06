7536398e4b02dd3206b5d812685108144fe125a0
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2024-05-06 23:42:10 +0200

    48 threads no flag

--- TOP-project/scripts/compare.sh
+++ TOP-project/scripts/compare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 i=24
-python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_Ofast_$i.txt > ../speed_up/sp_100_Ofast_$i.txt
-python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_Ofast_$i.txt > ../speed_up/sp_500_Ofast_$i.txt
-python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_Ofast_$i.txt > ../speed_up/sp_1000_Ofast_$i.txt
+python3 ../scripts/compare.py ../reference/ref_100.txt ../results/res_100_$i.txt > ../speed_up/scal_100_$i.txt
+python3 ../scripts/compare.py ../reference/ref_500.txt ../results/res_500_$i.txt > ../speed_up/scal_500_$i.txt
+python3 ../scripts/compare.py ../reference/ref_1000.txt ../results/res_1000_$i.txt > ../speed_up/scal_1000_$i.txt
 
